5336c3dd962b140c780d5c89f5261f6e8973c1a5
GilmarMMJr <gilmar_mendonca@live.com>
2022-03-20 21:31:58 -0300

    add protocol button

--- LoginViewCode-curso/LoginViewCode-curso/Login/VC/ViewController.swift
+++ LoginViewCode-curso/LoginViewCode-curso/Login/VC/ViewController.swift
@@ -18,12 +18,35 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.loginScreen?.delegate(delegate: self)
+        self.loginScreen?.configTextFieldDelegate(delegate: self)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.setNavigationBarHidden(true, animated: false)
     }
+    
+}
+
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+    }
+    
+}
+
+extension ViewController: LoginScreenProtocol {
+    
+    func actionLoginButton() {
+        print("Deu bom login !!!")
+    }
+    
+    func actionRegisterButton() {
+        print("Deu bom o registro !!!")
+    }
+    
     
 }
 
